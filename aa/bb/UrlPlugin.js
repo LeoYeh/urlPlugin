4b8fcacbfea610556d0c2ddaf9d37395d9a641eb
homexd@gmail.com <zaq1ZAQ!>
2016-07-08 19:32:11 +0800

    [add] location.hashs

--- aa/bb/UrlPlugin.js
+++ aa/bb/UrlPlugin.js
@@ -1,17 +1,19 @@
 var UrlPlugin = (function () {
     function UrlPlugin() {
         this.entireUrl = location.href;
-        this.sec = this.entireUrl.split("//")[1].split("/");
+        this.sec = this.entireUrl.split("//")[1].split("?")[0].split("#")[0].split("/");
         this.fileName = this.entireUrl.split("/").pop().split('?')[0].split('#')[0];
-        this.params = this.getParams();
+        this.params = this.getParams(location.search);
+        this.hashs = this.getParams(location.hash, ";");
         location['sec'] = this.sec;
         location['fileName'] = this.fileName;
         location['params'] = this.params;
+        location['hashs'] = this.hashs;
     }
-    UrlPlugin.prototype.getParams = function () {
-        var path = location.search;
+    UrlPlugin.prototype.getParams = function (path, character) {
+        if (character === void 0) { character = "&"; }
         path = path.slice(1, path.length);
-        var list = path.split("&");
+        var list = path.split(character);
         var rows = [];
         for (var i in list) {
             rows.push(list[i]);
--- aa/bb/UrlPlugin.ts
+++ aa/bb/UrlPlugin.ts
@@ -4,19 +4,21 @@ class UrlPlugin {
 	r:string; root:string;
 	sec:string[];
 	params:{key:string,val:string};
+	hashs:{key:string,val:string};
     constructor() { 
         this.entireUrl = location.href;
-        this.sec = this.entireUrl.split("//")[1].split("/");
+        this.sec = this.entireUrl.split("//")[1].split("?")[0].split("#")[0].split("/");
         this.fileName = this.entireUrl.split("/").pop().split('?')[0].split('#')[0];
-        this.params = this.getParams();
+        this.params = this.getParams(location.search);
+        this.hashs = this.getParams(location.hash,";");
         location['sec'] = this.sec;
         location['fileName'] = this.fileName;
         location['params'] = this.params;
+        location['hashs'] = this.hashs;
     }
-    private getParams():{key:string,val:string}{
-	    let path:string = location.search;
+    public getParams(path:string, character:string ="&"):{key:string,val:string}{
 	    path = path.slice( 1, path.length );
-	    let list:string[] = path.split("&");
+	    let list:string[] = path.split(character);
 	    let rows:any = [];
 	    for( var i in list ){ rows.push( list[i] );  }
 	    let obj:any = {};
